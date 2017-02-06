eaea5c04011e740d34b157cdcb5a89d017b6ac0e
AeonSake <aeon_sake@hotmail.com>
2017-02-06 20:58:26 +0100

    testing

--- src/user.js
+++ src/user.js
@@ -69,6 +69,7 @@ module.exports = (app) => {
         });
       }
       else {
+        console.log(data);
         for (var i = 0; i < data.members.length; i++) {
           user_db[i] = {
             id: data.members[i].id,
