d9dfbc87655aa255c61fea34c0aa5017349852b5
AeonSake <aeon_sake@hotmail.com>
2017-03-10 11:50:17 +0100

    testing

--- src/config.js
+++ src/config.js
@@ -48,13 +48,13 @@ let config = module.exports = {
   
   // Destiny config
   destiny_key: process.env.destiny_key,
-  destiny_ch: (process.env.destiny_ch ? process.env.destiny_ch : this.bot_ch),
+  destiny_ch: (process.env.destiny_ch !== undefined ? process.env.destiny_ch : this.bot_ch),
   
   // Poll config
-  poll_ch: (process.env.poll_ch ? process.env.poll_ch : this.bot_ch),
+  poll_ch: (process.env.poll_ch !== undefined ? process.env.poll_ch : this.bot_ch),
   
   // Events config
-  event_ch: (process.env.event_ch ? process.env.event_ch : this.bot_ch),
+  event_ch: (process.env.event_ch !== undefined ? process.env.event_ch : this.bot_ch),
   
   
   
