bd791b6dbc44a956783205c84e48eadc8cc8c8ac
AeonSake <aeon_sake@hotmail.com>
2017-03-17 13:18:28 +0100

    testing

--- src/destiny.js
+++ src/destiny.js
@@ -953,8 +953,8 @@ module.exports = (app) => {
   
   slapp.command('/destiny', "update", (msg, cmd) => {
     if (msg.body.user_id == config.admin_id) {
-      console.log(moment("18:00").add(15, 'm').format());
-      console.log(moment("18:00").add(15, 'm').format("mm HH"));
+      console.log(moment("19:00", "HH:mm").add(15, 'm').format());
+      console.log(moment("19:00", "HH:mm").add(15, 'm').format("mm HH"));
       
       resetSchedules(msg);
       getDefinitions(getActivities);
