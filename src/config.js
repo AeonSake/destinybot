7e27e36e1cd74ca30ef577b89a9beaf46d05770a
AeonSake <aeon_sake@hotmail.com>
2017-04-23 16:45:42 +0200

    new layout for list and soon messages

--- src/config.js
+++ src/config.js
@@ -34,7 +34,7 @@ let config = module.exports = {
   bb_persist_provider: process.env.PERSIST_PROVIDER || 'beepboop',
   
   // Project config
-  version: "6.0.9",
+  version: "6.0.10",
   title: "DestinyBot",
   admin_id: process.env.admin_id,
   admin_ch: "",
