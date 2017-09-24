32c33222214a08f84ec8f03046591669afc32f4d
AeonSake <aeon_sake@hotmail.com>
2017-09-24 16:14:00 +0200

    temp. disabling destiny plugin

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ var app = {
   team
 };
 
-require('./src/destiny')(app);
+//require('./src/destiny')(app);
 require('./src/events')(app);
 require('./src/polls')(app);
 require('./src/bdays')(app);
